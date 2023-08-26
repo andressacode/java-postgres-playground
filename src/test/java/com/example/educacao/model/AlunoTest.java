faeda95cdd81b553f8d0ba04cc3c1d9f98760656
Andressa Silva de Souza <andressaszx@gmail.com>
2023-08-26 14:58:40 +0000

    Enviando exercicio 1 completo.

--- src/test/java/com/example/educacao/model/AlunoTest.java
+++ src/test/java/com/example/educacao/model/AlunoTest.java
@@ -11,13 +11,24 @@ public class AlunoTest {
         Aluno aluno = new Aluno();
 
         aluno.setNota1(10.0);
+        aluno.setNota2(8.0);
+        aluno.setNota3(8.0);
+        double esperado = 8.66;
+        double obtido = aluno.calculaMedia();
+        assertEquals(esperado, obtido, 0.01);
+
+    }
+
+    @Test
+    void calculaMediaValoresIguais() {
+        Aluno aluno = new Aluno();
+
+        aluno.setNota1(10.0);
         aluno.setNota2(10.0);
         aluno.setNota3(10.0);
-
         double esperado = 10;
         double obtido = aluno.calculaMedia();
-
-        assertEquals(esperado, obtido);
+        assertEquals(esperado, obtido, 0.01);
 
     }
 }
